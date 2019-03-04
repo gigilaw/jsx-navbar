b9bb3089b993b8ad90c67d85e535ab2d8b8bf04d
Gigi Law <gigi.law.14@gmail.com>
2019-03-04 11:23:08 +0800

    Initial commit

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Contact = () => {
+	return (
+		<div className="container">
+			<h4 className="center">Contact</h4>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet veritatis
+				libero dolores illum beatae autem, rerum blanditiis nobis reprehenderit
+				ipsum nisi aliquid, facere quae doloribus labore. Enim assumenda
+				asperiores facilis?
+			</p>
+		</div>
+	)
+}
+
+export default Contact
