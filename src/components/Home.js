962078f935ef76328767e0986707e2ab70369e61
Gigi Law <gigi.law.14@gmail.com>
2019-03-05 10:55:56 +0800

    Connect Redux store

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,10 @@
 import React, { Component } from 'react'
-import axios from 'axios'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 class Home extends Component {
-	state = {
-		posts: [],
-	}
-	componentDidMount() {
-		axios.get('https://jsonplaceholder.typicode.com/posts').then(res => {
-			this.setState({
-				posts: res.data.slice(0, 10),
-			})
-		})
-	}
 	render() {
-		const { posts } = this.state
+		const { posts } = this.props
 		const postList = posts.length ? (
 			posts.map(post => {
 				return (
@@ -39,5 +29,10 @@ class Home extends Component {
 		)
 	}
 }
+const mapStateToProps = state => {
+	return {
+		posts: state.posts,
+	}
+}
 
-export default Home
+export default connect(mapStateToProps)(Home)
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,5 +1,24 @@
 const initState = {
-	posts: [],
+	posts: [
+		{
+			id: '1',
+			title: 'AAA',
+			body:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente vero in nesciunt facere animi laudantium eius distinctio dignissimos? Impedit dolorum natus modi quos sunt cumque cum unde nobis, quo velit?',
+		},
+		{
+			id: '2',
+			title: 'BBB',
+			body:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente vero in nesciunt facere animi laudantium eius distinctio dignissimos? Impedit dolorum natus modi quos sunt cumque cum unde nobis, quo velit?',
+		},
+		{
+			id: '3',
+			title: 'CCC',
+			body:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente vero in nesciunt facere animi laudantium eius distinctio dignissimos? Impedit dolorum natus modi quos sunt cumque cum unde nobis, quo velit?',
+		},
+	],
 }
 
 const rootReducer = (state = initState, action) => {
